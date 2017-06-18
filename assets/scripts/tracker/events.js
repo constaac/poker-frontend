59289d19a63a29f43a0f763ffb19fc94762de78b
constaac <aconstantine719@gmail.com>
2017-06-18 18:30:27 -0400

    Condensed Set Seat Handler applications

--- assets/scripts/tracker/events.js
+++ assets/scripts/tracker/events.js
@@ -4,15 +4,11 @@ const ui = require('./ui.js')
 const logic = require('./logic.js')
 
 const addHandlers = () => {
-  $('#seat-button-2').on('click', ui.onSetSeat2)
-  $('#seat-button-3').on('click', ui.onSetSeat3)
-  $('#seat-button-4').on('click', ui.onSetSeat4)
-  $('#seat-button-5').on('click', ui.onSetSeat5)
-  $('#seat-button-6').on('click', ui.onSetSeat6)
-  $('#seat-button-7').on('click', ui.onSetSeat7)
-  $('#seat-button-8').on('click', ui.onSetSeat8)
-  $('#seat-button-9').on('click', ui.onSetSeat9)
-  $('#seat-button-10').on('click', ui.onSetSeat10)
+  for (let j = 1; j <= 10; j++) {
+    $('#seat-button-' + j).on('click', () => {
+      ui.onSetSeatButton(j)
+    })
+  }
   $('#start-round-btn').on('click', logic.onStartRound)
   $('#set-table-btn').on('click', ui.openSetSeats)
   $('#teststats').on('click', logic.teststats)
--- assets/scripts/tracker/ui.js
+++ assets/scripts/tracker/ui.js
@@ -48,106 +48,22 @@ const openSetSeats = () => {
   $('#tableModal').modal('toggle')
 }
 
-const onSetSeat2 = () => {
+const onSetSeatButton = (count) => {
   resetSeats()
   $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2] })
+  const tempArray = []
+  for (let i = 1; i <= count; i++) {
+    tempArray.push(i)
+  }
+  const showSeatsHtml = setSeatsNumber({ list: tempArray })
   $('#seats-table').empty()
   $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(2)
-  setOnCheckRadio(2)
-}
-
-const onSetSeat3 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(3)
-  setOnCheckRadio(3)
-}
-
-const onSetSeat4 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(4)
-  setOnCheckRadio(4)
-}
-
-const onSetSeat5 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(5)
-  setOnCheckRadio(5)
-}
-
-const onSetSeat6 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5, 6] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(6)
-  setOnCheckRadio(6)
-}
-
-const onSetSeat7 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5, 6, 7] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(7)
-  setOnCheckRadio(7)
-}
-
-const onSetSeat8 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5, 6, 7, 8] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(8)
-  setOnCheckRadio(8)
-}
-
-const onSetSeat9 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5, 6, 7, 8, 9] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(9)
-  setOnCheckRadio(9)
-}
-
-const onSetSeat10 = () => {
-  resetSeats()
-  $('.load-save-menu').css('display', 'block')
-  const showSeatsHtml = setSeatsNumber({ list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] })
-  $('#seats-table').empty()
-  $('#seats-table').append(showSeatsHtml)
-  setOnCheckBox(10)
-  setOnCheckRadio(10)
+  setOnCheckBox(count)
+  setOnCheckRadio(count)
 }
 
 module.exports = {
-  onSetSeat2,
-  onSetSeat3,
-  onSetSeat4,
-  onSetSeat5,
-  onSetSeat6,
-  onSetSeat7,
-  onSetSeat8,
-  onSetSeat9,
-  onSetSeat10,
+  onSetSeatButton,
   openSetSeats,
   resetSeats
 }
