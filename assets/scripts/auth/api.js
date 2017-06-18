ca9eb88b681d9981e7dfd304c3841aaae69e022e
constaac <aconstantine719@gmail.com>
2017-06-18 18:16:11 -0400

    Changed URL variable in Auth API

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -5,7 +5,7 @@ const store = require('../store')
 
 const signUp = function (data) {
   return $.ajax({
-    url: config.apiOrigins.development + '/sign-up',
+    url: config.apiOrigin + '/sign-up',
     method: 'POST',
     data
   })
@@ -13,7 +13,7 @@ const signUp = function (data) {
 
 const signIn = function (data) {
   return $.ajax({
-    url: config.apiOrigins.development + '/sign-in',
+    url: config.apiOrigin + '/sign-in',
     method: 'POST',
     data
   })
@@ -30,7 +30,7 @@ const logout = function (data) {
     headers: {
       'Authorization': 'Token token=' + store.userToken
     },
-    url: config.apiOrigins.development + '/sign-out/' + store.userID,
+    url: config.apiOrigin + '/sign-out/' + store.userID,
     method: 'DELETE'
   })
     .then(function () {
@@ -44,7 +44,7 @@ const changePassword = function (data) {
     headers: {
       'Authorization': 'Token token=' + store.userToken
     },
-    url: config.apiOrigins.development + '/change-password/' + store.userID,
+    url: config.apiOrigin + '/change-password/' + store.userID,
     method: 'PATCH',
     data
   })
